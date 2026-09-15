3ca186cdd29af9f6ff1f2c87051d29dbd562774e
Gabriel Rumpel <gabriel.rumpel@ufn.edu.br>
2026-09-15 09:04:23 -0300

    Fix and complete program to read full names

--- programas/desafio2_14.cpp
+++ programas/desafio2_14.cpp
@@ -5,9 +5,40 @@
 #include <iostream>
 #include <string> 
 #include <vector>
+#define N 5
 
 using namespace std;
 
-typdefstruct{
+typedef struct{
+    string nomeCompleto;
+    string sobreNome;
+} Pessoa;
 
+string extrairSobrenome(string nomeCompleto){
+    int posicao = nomeCompleto.find_last_of(' ');
+
+    return nomeCompleto.substr(posicao + 1);
+}
+
+int main(){
+    
+    vector<Pessoa> pessoas(N);
+
+    cin.ignore();
+    
+    for(int i = 0; i < N; i++){
+        cout << "\nDigite o nome completo da pessoa " << i + 1 << ": ";
+        getline(cin, pessoas[i].nomeCompleto);
+
+        pessoas[i].sobreNome = extrairSobrenome(pessoas[i].nomeCompleto);
+    }
+
+    for (int i = 0; i < N; i++){
+        cout << "\nPessoa " << i+1 << endl;
+        cout << "Nome completo: " << pessoas[i].nomeCompleto << endl;
+        cout << "Sobrenome: " << pessoas[i].sobreNome << endl;
+    }
+
+    
+    return 1;
 }
